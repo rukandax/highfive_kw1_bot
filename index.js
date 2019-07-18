d8f20fb667942c9d85151dc1f44f4713dcb15ac7
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2019-07-18 15:43:56 +0700

    Update

--- index.js
+++ index.js
@@ -1,123 +1,89 @@
 require('dotenv').config();
 
 const Telegraf = require('telegraf');
-const Loki = require('lokijs');
 
-const db = new Loki('loki.json', {
-  autoload: true,
-  autoloadCallback: databaseInitialize,
-  autosave: true, 
-  autosaveInterval: 4000,
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  return ctx.reply('Gunakan format highfive seperti biasa tanpa kode kategori.');
 });
 
-let config = null;
-
-function databaseInitialize() {
-  config = db.getCollection('config');
-
-  if (config === null) {
-    config = db.addCollection('config');
+bot.command('showchatid', (ctx) => {
+  if (ctx.message.from.username !== 'rukandax') {
+    return ctx.reply('Anda bukan admin.');
   }
 
-  init();
-}
+  return ctx.reply(`chat_id = ${ctx.message.chat.id}`);
+});
 
-function init() {
-  const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.command('highfive', (ctx) => {
+  if (ctx.message.from.username !== 'highfive_kw1_bot') {
+    return ctx.reply('Nice try.');
+  }
 
-  bot.start((ctx) => {
-    return ctx.reply('Gunakan format highfive seperti biasa tanpa kode kategori.');
-  });
-  
-  bot.command('getchatid', (ctx) => {
-    if (ctx.message.from.username !== 'rukandax') {
-      return ctx.reply('Anda bukan admin.');
+  let pushToUser = true;
+  const users = [];
+
+  let poin = null;
+  let message = '';
+
+  let text = ctx.message.text.replace('/highfive', '').trim();
+  let textArray = text.split(' ');
+
+  for (textContent of textArray) {
+    textContent = textContent.trim();
+
+    if (textContent.includes('@') && textContent.length > 1 && pushToUser) {
+      users.push(textContent);
+    } else if (poin === null && !isNaN(parseInt(textContent))) {
+      pushToUser = false;
+      poin = parseInt(textContent);
+    } else {
+      pushToUser = false;
+      message += textContent + ' ';
     }
+  }
 
-    const group = config.find({ chat_id: config.get(1).chat_id });
-    group.chat_id = ctx.message.chat.id;
+  if (poin === null || message.length <= 0 || users.length <= 0) {
+    return ctx.reply('Format salah, gunakan format highfive seperti biasa tanpa kode kategori.');
+  }
 
-    config.update(group);
-    return ctx.reply(`Berhasil menyimpan chat_id ${ctx.message.chat.id}`);
-  });
+  let type = 'Nice';
 
-  bot.command('showchatid', (ctx) => {
-    if (ctx.message.from.username !== 'rukandax') {
-      return ctx.reply('Anda bukan admin.');
-    }
-
-    return ctx.reply(`chat_id ${config.get(1).chat_id}`);
-  });
+  if (poin < 0) {
+    type = 'Hehehe';
+  }
   
-  bot.command('highfive', (ctx) => {
-    if (ctx.message.from.username !== 'highfive_kw1_bot') {
+  if (poin >= 5) {
+    type = 'Cool';
+  }
+
+  if (poin >= 10) {
+    type = 'Great';
+  }
+
+  if (poin >= 15) {
+    type = 'Fantastic';
+  }
+
+  if (poin > 20) {
+    type = 'Uwoww';
+  }
+
+  if (poin > 5000) {
+    type = 'Wadaww';
+  }
+
+  if (poin > 1000) {
+    type = 'Wadidaww';
+  }
+
+  const output = `${type} highfive! @${ctx.message.from.username} berbagi ${users.length > 1 ? 'masing-masing ' : ''}<b>${poin}</b> poin untuk:\n${users.join('\n')}\nkarena <b>${message.trim()}</b>`;
+  return ctx.replyWithHTML(output, { chat_id: -1001113266099 })
+    .catch((e) => {
+      console.log(e);
       return ctx.reply('Nice try.');
-    }
+    });
+});
 
-    let pushToUser = true;
-    const users = [];
-  
-    let poin = null;
-    let message = '';
-  
-    let text = ctx.message.text.replace('/highfive', '').trim();
-    let textArray = text.split(' ');
-  
-    for (textContent of textArray) {
-      textContent = textContent.trim();
-  
-      if (textContent.includes('@') && textContent.length > 1 && pushToUser) {
-        users.push(textContent);
-      } else if (poin === null && !isNaN(parseInt(textContent))) {
-        pushToUser = false;
-        poin = parseInt(textContent);
-      } else {
-        pushToUser = false;
-        message += textContent + ' ';
-      }
-    }
-  
-    if (poin === null || message.length <= 0 || users.length <= 0) {
-      return ctx.reply('Format salah, gunakan format highfive seperti biasa tanpa kode kategori.');
-    }
-
-    let type = 'Nice';
-
-    if (poin < 0) {
-      type = 'Hehehe';
-    }
-    
-    if (poin >= 5) {
-      type = 'Cool';
-    }
-
-    if (poin >= 10) {
-      type = 'Great';
-    }
-
-    if (poin >= 15) {
-      type = 'Fantastic';
-    }
-
-    if (poin > 20) {
-      type = 'Uwoww';
-    }
-
-    if (poin > 200) {
-      type = 'Wadaww';
-    }
-
-    if (poin > 500) {
-      type = 'Wadidaww';
-    }
-  
-    const output = `${type} highfive! @${ctx.message.from.username} berbagi ${users.length > 1 ? 'masing-masing ' : ''}<b>${poin}</b> poin untuk:\n${users.join('\n')}\nkarena <b>${message.trim()}</b>`;
-    return ctx.replyWithHTML(output, { chat_id: -1001113266099 })
-      .catch((e) => {
-        console.log(e);
-        return ctx.reply('Nice try.');
-      });
-  });
-  
-  bot.launch()
-}
+bot.launch();
