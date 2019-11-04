4c6fd8c2b792d885c8cef7f721ce15c70825a0ae
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2019-11-04 15:57:47 +0700

    Change instagram search service

--- library/instagram.js
+++ library/instagram.js
@@ -52,48 +52,16 @@ const findInstagram = async (ctx, target = '') => {
     });
   });
 
-  const links = await axios.get(`https://gramusers.com/search/${name}`)
+  const links = await axios.get(`http://picpanzee.com/search/${name}`)
     .then(({ data }) => {
       const users = [];
 
       const $ = cheerio.load(data);
-      const usersEl = $('.timg');
+      const usersEl = $('.grid-user-wrapper a');
 
       usersEl.each((_, el) => {
-        const username = $(el).attr('href').replace('http://gramusers.com/user/', '');
-
-        const suffixes = ["", "k", "m", "b","t"];
-
-        let followers = $(el).html().match(/([0-9.kmbt]+) follower/)[1];
-
-        suffixes.forEach((abbr) => {
-          if (typeof followers === 'string' && followers.includes(abbr)) {
-            if (abbr === 'k') {
-              followers = followers.replace(abbr, '');
-              followers = parseInt(followers) * 1000;
-            }
-
-            if (abbr === 'm') {
-              followers = followers.replace(abbr, '');
-              followers = parseInt(followers) * 1000000;
-            }
-
-            if (abbr === 'b') {
-              followers = followers.replace(abbr, '');
-              followers = parseInt(followers) * 1000000000;
-            }
-
-            if (abbr === 't') {
-              followers = followers.replace(abbr, '');
-              followers = parseInt(followers) * 1000000000000;
-            }
-          }
-        })
-
-        users.push({
-          username,
-          followers: parseInt(followers),
-        });
+        const username = $(el).attr('href').replace('http://picpanzee.com/', '');
+        users.push(username);
       });
 
       return users;
@@ -106,20 +74,10 @@ const findInstagram = async (ctx, target = '') => {
     })
 
   if (links.length > 0) {
-    for (let u = 0;u < links.length; u += 1) {
-      for (let i = 0;i < links.length; i += 1) {
-        if (links[i + 1] && links[i].followers < links[i + 1].followers) {
-          const a = links[i];
-          links[i] = links[i + 1];
-          links[i + 1] = a;
-        }
-      }
-    }
-
     let users = [];
 
     for (let i = 0;i < links.length; i += 1) {
-      users.push(`https://www.instagram.com/${links[i].username}`);
+      users.push(`https://www.instagram.com/${links[i]}`);
     }
 
     users = users.slice(0, 20);
