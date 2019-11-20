46f76caf2fe2af35f488bb75202169b090274218
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2019-11-20 19:43:04 +0700

    Update

--- index.js
+++ index.js
@@ -1,138 +1,185 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-require('dotenv').config();
+require('dotenv').config()
 
-const fs = require('fs');
-const Telegraf = require('telegraf');
+const fs = require('fs')
+const Telegraf = require('telegraf')
 const Schedule = require('node-schedule')
-const axios = require('axios');
-const puppeteer = require('puppeteer');
-const { encode, decode } = require('jwt-simple');
+const axios = require('axios')
+const puppeteer = require('puppeteer')
+const { encode, decode } = require('jwt-simple')
 
 const {
   greeting
-} = require('./library/general');
+} = require('./library/general')
 
 const {
   findInstagram
-} = require('./library/instagram');
+} = require('./library/instagram')
 
 const {
   getMostLikedIgPost
-} = require('./library/mostlikedigpost');
+} = require('./library/mostlikedigpost')
 
-const CORE_HOUR_END = '📢 Teet teet teet~ core hour udah berakhir~';
+const CORE_HOUR_END = '📢 Teet teet teet~ core hour udah berakhir~'
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
 Schedule.scheduleJob('payday', '0 11 * * 1-5', () => {
-  const today = new Date();
+  const today = new Date()
 
-  const dayBeforePayday = new Date();
-  dayBeforePayday.setDate(26);
+  const dayBeforePayday = new Date()
+  dayBeforePayday.setDate(26)
 
-  const payday = new Date();
-  payday.setDate(27);
+  const payday = new Date()
+  payday.setDate(27)
   
   if(payday.getDay() == 0) {
-    dayBeforePayday.setDate(payday.getDate() - 3);
+    dayBeforePayday.setDate(payday.getDate() - 3)
   } else if (payday.getDay() == 6) {
-    dayBeforePayday.setDate(payday.getDate() - 2);
+    dayBeforePayday.setDate(payday.getDate() - 2)
   }
 
   if (today.getDate() === dayBeforePayday.getDate()) {
-    const message = '📢 Besok gajian gaesss~~';
+    const message = '📢 Besok gajian gaesss~~'
     
     bot.telegram.sendMessage(-1001430743348, message).catch((err) => {
-      console.log(err);
-    });
+      console.log(err)
+    })
   }
-});
+})
 
 Schedule.scheduleJob('endCoreHour', '0 17 * * 1-5', () => {
   bot.telegram.sendMessage(-1001430743348, CORE_HOUR_END).catch((err) => {
-    console.log(err);
-  });
+    console.log(err)
+  })
 })
 
-let isWebDown = false;
+let isWebDown = false
 Schedule.scheduleJob('upMonitor', '*/5 * * * *', () => {
   const message = 'Bukalapak down ya ?'
 
   axios.get('https://www.bukalapak.com/version.txt')
     .then(({ data }) => {
       if (data.trim().length === 40) {
-        isWebDown = false;
+        isWebDown = false
       } else {
         if (!isWebDown) {
-          isWebDown = true;
+          isWebDown = true
   
           bot.telegram.sendMessage(-1001430743348, message).catch((err) => {
-            console.log(err);
-          });
+            console.log(err)
+          })
         }
       }
     })
     .catch(() => {
       if (!isWebDown) {
-        isWebDown = true;
+        isWebDown = true
 
         bot.telegram.sendMessage(-1001430743348, message).catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
       }
-    });
+    })
 })
 
-bot.start(greeting);
-bot.command('help', greeting);
-bot.command('instagram', findInstagram);
-bot.command('mostlikedigpost', getMostLikedIgPost);
+bot.start(greeting)
+bot.command('help', greeting)
+bot.command('instagram', findInstagram)
+bot.command('mostlikedigpost', getMostLikedIgPost)
 
 bot.command('beautymeter', (ctx) => {
   ctx.reply('Mana nih foto nya bosque ??', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-    console.log(err);
-  });
+    console.log(err)
+  })
 })
 
 bot.command('shout', (ctx) => {
   ctx.replyWithHTML('Mau ngirim apa bosque ??', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-    console.log(err);
-  });
+    console.log(err)
+  })
 })
 
 bot.command('deleteshout', (ctx) => {
-  let text = '';
+  let text = ''
 
   if (ctx.message.text.includes('/deleteshout@highfive_kw1_bot')) {
-    text = ctx.message.text.replace('/deleteshout@highfive_kw1_bot', '').trim();
+    text = ctx.message.text.replace('/deleteshout@highfive_kw1_bot', '').trim()
   } else {
-    text = ctx.message.text.replace('/deleteshout', '').trim();
+    text = ctx.message.text.replace('/deleteshout', '').trim()
   }
 
   if (!text.length) {
     return ctx.reply('Pesan yang mau dihapus tidak ditemukan. JWT masih kosong.', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-      console.log(err);
-    });
+      console.log(err)
+    })
   }
 
-  let decoded = '';
+  let decoded = ''
   try {
-    decoded = decode(text, process.env.BOT_TOKEN);
+    decoded = decode(text, process.env.BOT_TOKEN)
   } catch (_) {
     ctx.reply('Pesan yang mau dihapus tidak ditemukan. JWT tidak valid.', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-      console.log(err);
-    });
+      console.log(err)
+    })
   }
 
   if (decoded > 0) {
     return bot.telegram.deleteMessage(-1001430743348, parseInt(decoded)).catch(() => {
       ctx.reply('Pesan yang mau dihapus tidak ditemukan.', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-        console.log(err);
-      });
-    });
+        console.log(err)
+      })
+    })
   } else {
-    console.log(decoded);
+    console.log(decoded)
+  }
+})
+
+bot.on('voice', (ctx) => {
+  console.log(ctx.message)
+
+  if (ctx.message.voice && ctx.message.chat.type === 'private') {
+    ctx.reply('Sebentar ya..', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
+      console.log(err)
+    })
+
+    const voiceLink = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+
+    let voiceExt = voiceLink.split('.')
+    voiceExt = voiceExt[voiceExt.length - 1]
+  
+    const voicePath = await axios({
+      url: voiceLink,
+      method: 'GET',
+      responseType: 'arraybuffer',
+    }).then(({ data }) => {
+      const outputFilename = `/tmp/highfive_kw1_bot_voice-${Date.now()}.${photoExt}`
+      fs.writeFileSync(outputFilename, data)
+  
+      return outputFilename
+    })
+
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
+    const page = await browser.newPage()
+  
+    await page.setRequestInterception(true)
+  
+    page.on('request', interceptedRequest => {
+      if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg') || interceptedRequest.url().endsWith('.jpeg') || interceptedRequest.url().endsWith('.css'))
+        interceptedRequest.abort()
+      else
+        interceptedRequest.continue()
+    })
+
+    await page.goto('https://www.audiospeedchanger.com/', {
+      timeout: 3000000
+    })
+  
+    const input = await page.$('#localfile')
+    await input.uploadFile(voicePath)
+
+    await browser.close()
   }
 })
 
@@ -141,8 +188,8 @@ bot.on('photo', async (ctx) => {
   
   if (!photo) {
     return ctx.reply('Duh mataku kelilipan 😷 coba kirimin lagi gambarnya', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-      console.log(err);
-    });
+      console.log(err)
+    })
   }
 
   if (
@@ -153,56 +200,56 @@ bot.on('photo', async (ctx) => {
     ctx.message.reply_to_message.text === 'Mana nih foto nya bosque ??'
   ) {
     ctx.reply('Sebentar ya, aku perhatiin baik-baik dulu..', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-      console.log(err);
-    });
+      console.log(err)
+    })
   
     const photoLink = await ctx.telegram.getFileLink(photo.file_id)
   
-    let photoExt = photoLink.split('.');
-    photoExt = photoExt[photoExt.length - 1];
+    let photoExt = photoLink.split('.')
+    photoExt = photoExt[photoExt.length - 1]
   
     const photoPath = await axios({
       url: photoLink,
       method: 'GET',
       responseType: 'arraybuffer',
     }).then(({ data }) => {
-      const outputFilename = `/tmp/highfive_kw1_bot_image.${photoExt}`;
-      fs.writeFileSync(outputFilename, data);
+      const outputFilename = `/tmp/highfive_kw1_bot_image-${Date.now()}.${photoExt}`
+      fs.writeFileSync(outputFilename, data)
   
-      return outputFilename;
-    });
+      return outputFilename
+    })
   
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
+    const page = await browser.newPage()
   
-    await page.setRequestInterception(true);
+    await page.setRequestInterception(true)
   
     page.on('request', interceptedRequest => {
       if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg') || interceptedRequest.url().endsWith('.jpeg') || interceptedRequest.url().endsWith('.css'))
-        interceptedRequest.abort();
+        interceptedRequest.abort()
       else
-        interceptedRequest.continue();
-    });
+        interceptedRequest.continue()
+    })
   
     await page.goto('https://hotness.ai/', {
       timeout: 3000000
-    });
+    })
   
     const input = await page.$('#imgFile')
-    let score = 0;
+    let score = 0
   
     if (input) {
       await input.uploadFile(photoPath)
       
       await page.waitForFunction(
         'document.querySelector("#hotText").innerText.includes("Your Attractiveness Score is") || document.querySelector("#hotText").innerText.includes("Error")',
-      );
+      )
     
       score = await page.evaluate(() => {
-        return document.querySelector("#hotText").innerText.replace('Your Attractiveness Score is ', '').replace(' out of 10', '');
-      });
+        return document.querySelector("#hotText").innerText.replace('Your Attractiveness Score is ', '').replace(' out of 10', '')
+      })
     } else {
-      const selector = '.onp-sl-social-button-twitter-tweet';
+      const selector = '.onp-sl-social-button-twitter-tweet'
   
       await page.$(selector)
       await page.evaluate((selector) => {
@@ -211,31 +258,31 @@ bot.on('photo', async (ctx) => {
         if (button) {
           button.click()
         }
-      }, selector); 
+      }, selector) 
   
       ctx.reply('Duh mataku kelilipan 😷 coba kirimin lagi gambarnya', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-        console.log(err);
-      });
+        console.log(err)
+      })
     }
   
-    await browser.close();
+    await browser.close()
   
-    let response = 'Mukanya yang sebelah mana sih ? Gak jelas.. 😒';
+    let response = 'Mukanya yang sebelah mana sih ? Gak jelas.. 😒'
   
     if (score >= 0) {
-      response = '😨 Ihh.. Jelek.. 😨';
+      response = '😨 Ihh.. Jelek.. 😨'
     }
   
     if (score >= 5) {
-      response = 'Hmmm, biasa aja sih 😌 yang kayak gini mah pasaran';
+      response = 'Hmmm, biasa aja sih 😌 yang kayak gini mah pasaran'
     }
   
     if (score >= 7) {
-      response = 'Lumayan lah.. Masih cocok dibawa-bawa ketemu mantan 😄';
+      response = 'Lumayan lah.. Masih cocok dibawa-bawa ketemu mantan 😄'
     }
   
     if (score >= 9) {
-      response = 'Kiw.. Kiw.. Bisa kali~~ 😎😍';
+      response = 'Kiw.. Kiw.. Bisa kali~~ 😎😍'
     }
   
     if (!isNaN(parseInt(score))) {
@@ -254,7 +301,7 @@ bot.on('message', (ctx) => {
       &&
     ctx.message.reply_to_message.text === 'Mau cari instagram siapa ?'
   ) {
-    findInstagram(ctx, ctx.message.text);
+    findInstagram(ctx, ctx.message.text)
   }
 
   if (
@@ -264,7 +311,7 @@ bot.on('message', (ctx) => {
       &&
     ctx.message.reply_to_message.text === 'Username instagram nya siapa ?'
   ) {
-    getMostLikedIgPost(ctx, ctx.message.text);
+    getMostLikedIgPost(ctx, ctx.message.text)
   }
 
   if (
@@ -282,64 +329,64 @@ bot.on('message', (ctx) => {
       return ctx.replyWithAnimation(ctx.message.animation.file_id, { chat_id: -1001430743348 })
         .then((res) => {
           ctx.replyWithHTML(`Berhasil mengirim pesan, gunakan perintah <code>/deleteshout ${encode(res.message_id, process.env.BOT_TOKEN)}</code> untuk menghapus pesan yang telah dikirim.\n\n<i>Hanya bisa menghapus pesan dengan durasi dibawah 48 jam.</i>`, { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-            console.log(err);
-          });
+            console.log(err)
+          })
 
           bot.telegram.sendAnimation(process.env.CONTROL_AREA, ctx.message.animation.file_id)
             .then(() => {
               bot.telegram.sendMessage(process.env.CONTROL_AREA, `Remove Command : /deleteshout ${encode(res.message_id, process.env.BOT_TOKEN)}`).catch((err) => {
-                console.log(err);
-              });
+                console.log(err)
+              })
             })
             .catch((err) => {
-              console.log(err);
-            });
+              console.log(err)
+            })
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     }
 
     if (ctx.message.sticker) {
       return ctx.replyWithSticker(ctx.message.sticker.file_id, { chat_id: -1001430743348 })
         .then((res) => {
           ctx.replyWithHTML(`Berhasil mengirim pesan, gunakan perintah <code>/deleteshout ${encode(res.message_id, process.env.BOT_TOKEN)}</code> untuk menghapus pesan yang telah dikirim.\n\n<i>Hanya bisa menghapus pesan dengan durasi dibawah 48 jam.</i>`, { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-            console.log(err);
-          });
+            console.log(err)
+          })
 
           bot.telegram.sendSticker(process.env.CONTROL_AREA, ctx.message.sticker.file_id)
             .then(() => {
               bot.telegram.sendMessage(process.env.CONTROL_AREA, `Remove Command : /deleteshout ${encode(res.message_id, process.env.BOT_TOKEN)}`).catch((err) => {
-                console.log(err);
-              });
+                console.log(err)
+              })
             })
             .catch((err) => {
-              console.log(err);
-            });
+              console.log(err)
+            })
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     }
 
     if (ctx.message.text) {
       return ctx.reply(`${ctx.message.text}`, { chat_id: -1001430743348 })
         .then((res) => {
           ctx.replyWithHTML(`Berhasil mengirim pesan, gunakan perintah <code>/deleteshout ${encode(res.message_id, process.env.BOT_TOKEN)}</code> untuk menghapus pesan yang telah dikirim.\n\n<i>Hanya bisa menghapus pesan dengan durasi dibawah 48 jam.</i>`, { reply_to_message_id: ctx.message.message_id }).catch((err) => {
-            console.log(err);
-          });
+            console.log(err)
+          })
   
           bot.telegram.sendMessage(process.env.CONTROL_AREA, `${res.text}\n\nRemove Command : /deleteshout ${encode(res.message_id, process.env.BOT_TOKEN)}`).catch((err) => {
-            console.log(err);
-          });
+            console.log(err)
+          })
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     }
 
-    return ctx.reply('Format belum didukung untuk shout', { reply_to_message_id: ctx.message.message_id });
+    return ctx.reply('Format belum didukung untuk shout', { reply_to_message_id: ctx.message.message_id })
   }
-});
+})
 
-bot.launch();
+bot.launch()
