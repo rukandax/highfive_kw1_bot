9ea9fa1a3ad5cae420b5d98a08e752790cdfa1b2
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2019-11-30 17:33:57 +0700

    Update

--- library/mostlikedigpost.js
+++ library/mostlikedigpost.js
@@ -60,6 +60,7 @@ async function getMostLikedIgPost(ctx, target = '') {
       console.log(err);
     });
   } catch (err) {
+    console.log(err)
     ctx.reply('Gak ketemu nih, mungkin akun nya di private..', { reply_to_message_id: ctx.message.message_id }).catch((err) => {
       console.log(err);
     });
